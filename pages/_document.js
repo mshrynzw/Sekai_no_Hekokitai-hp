11f5f2655e9ec87139f17b07652b7fa6179d4145
mshrynzw <iq87io25@gmail.com>
2023-04-13 11:14:22 +0900

    Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,47 @@
-import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react"
+import Document, { Html, Head, Main, NextScript } from "next/document"
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="theme-color" content="#000000" />
-          <link rel="shortcut icon" href="/img/brand/favicon.ico" />
+          <meta charSet="utf-8"/>
+          <meta name="theme-color" content="#000000"/>
+          <link rel="shortcut icon" href="/img/brand/favicon.ico"/>
           <link
             rel="apple-touch-icon"
             sizes="76x76"
             href="/img/brand/apple-icon.png"
           />
+          {/* Google Analytics */}
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+              }}
+            />
+          </>
         </Head>
         <body className="text-blueGray-700 antialiased">
-          <div id="page-transition"></div>
-          <Main />
-          <NextScript />
+        <div id="page-transition"></div>
+        <Main/>
+        <NextScript/>
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
